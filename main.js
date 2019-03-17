7eef0f79a70d1277f5bb160e1d53379df9738461
JackDing <361164217@qq.com>
2019-03-17 20:35:14 +0800

    js

---
+++ main.js
@@ -0,0 +1,12 @@
+work1.onclick = function () {
+    choice.className = 'bar state1'
+}
+work2.onclick = function () {
+    choice.className = 'bar state2'
+}
+work3.onclick = function () {
+    choice.className = 'bar state3'
+}
+
+
+
