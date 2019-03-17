6bf27bed9326f812b1570e0b809ecf27864d1013
JackDing <361164217@qq.com>
2019-03-17 21:53:14 +0800

    JS效果

--- main.js
+++ main.js
@@ -9,4 +9,24 @@ work3.onclick = function () {
 }
 
 
+document.onscroll = function () {
+    if (window.scrollY > 100) {
+        fixtop.classList.add('moving')
+    }
+    else {
+        fixtop.classList.remove('moving')
+    }
+}
+
+
+var progress = document.getElementsByClassName('progress')
+document.onscroll = function () {
+    var div = document.getElementById('skill')
+    if (window.scrollY > (div.offsetTop - 600)) {
+        for (i = 0; i < progress.length; i++) {
+            progress[i].classList.add('play')
+        }
+    }
+}
+
 
